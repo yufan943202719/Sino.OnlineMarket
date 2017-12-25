4e6fa1d732247b6113ef3a65d55675e2734af90b
yufan943202719 <34699227+yufan943202719@users.noreply.github.com>
2017-12-25 11:27:06 +0800

    第一次提交

---
+++ src/Sino.OnlineMarket.Webhost/ViewModel/BuyGoodsListResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sino.OnlineMarket.Webhost.ViewModel
+{
+    public class BuyGoodsListResponse
+    {
+
+        public List<BuyGoodsList> BuyGoodsList { get; set; }
+    }
+
+    public class BuyGoodsList
+    {
+        public int UserId { get; set; }
+        public string GoodsId { get; set; }
+        public int BuyStatus { get; set; }
+        public string  Datetime { get; set; }
+    }
+}
