6b0b7c079e5d2c330112aa115f27078c86f78650
gravinliu <836276616@qq.com>
2017-12-29 16:49:58 +0800

    第五次提交

---
+++ src/Sino.OnlineMarket.Repositories/Repository/TokenRepository.cs
@@ -0,0 +1,40 @@
+﻿using Sino.OnlineMarket.Repositories.ViewModel;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.IdentityModel.Tokens;
+using Sino.OnlineMarket.Webhost.Auth;
+using System.Linq;
+
+namespace Sino.OnlineMarket.Repositories.Repository
+{
+    public class TokenRepository
+    {
+
+        /// <summary>
+        /// 生成Token值
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="expires"></param>
+        /// <returns></returns>
+        public  string GenerateToken(string user, DateTime expires)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(user, "TokenAuth"), new[] { new Claim("EntityID", "1", ClaimValueTypes.Integer) });
+
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = TokenAuthOption.Issuer,
+                Audience = TokenAuthOption.Audience,
+                SigningCredentials = TokenAuthOption.SigningCredentials,
+                Subject = identity,
+                Expires = expires
+            });
+            return handler.WriteToken(securityToken);
+        }
+
+      
+    }
+}
---
+++ src/Sino.OnlineMarket.Webhost/Controllers/JwtUserLogin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Sino.OnlineMarket.Webhost.ViewModel;
+using Sino.OnlineMarket.Repositories.Repository;
+using Sino.OnlineMarket.Repositories.ViewModel;
+using Sino.OnlineMarket.Webhost.Auth;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sino.OnlineMarket.Webhost.Controllers
+{
+
+    /// <summary>
+    /// JWT用户登陆
+    /// </summary>
+    [Route("sino/[controller]")]
+    public class JwtUserLogin : Controller
+    {
+        private OnlineMarketContext DB = new OnlineMarketContext();
+        private TokenRepository tr = new TokenRepository();
+        UsersRepository ur = new UsersRepository();
+
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        [HttpPost("Login")]
+        public dynamic Login([FromBody] JwtUser body)
+        {
+            bool authenticated = false;
+            
+            int entityId = -1;
+            string jwtuser = null;
+            string token = null;
+            DateTime tokenExpires = default(DateTime);
+            var currentUser = HttpContext.User;
+            authenticated = currentUser.Identity.IsAuthenticated;
+                if (authenticated)
+                {
+                    jwtuser = currentUser.Identity.Name;
+                foreach (Claim c in currentUser.Claims)
+                {
+                    if (c.Type == "EntityID")
+                        entityId = Convert.ToInt32(c.Value);
+                }
+                    tokenExpires = DateTime.UtcNow.AddMinutes(2);
+                    token = tr.GenerateToken(body.UserName, tokenExpires);
+                }
+            return new { authenticated = authenticated, jwtuser = body.UserName, entityId = entityId, token = token, tokenExpires = tokenExpires };
+            /*
+            
+
+            return $"Hello! {HttpContext.User.Identity.Name}, your ID is:{id}";
+            */
+        }
+
+        /// <summary>
+        /// 重置一个新的Token值
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost("GetAuthToken")]
+        public dynamic GetAuthToken([FromBody] JwtUser user)
+        {
+            var existUser = DB.Users.FirstOrDefault(u => u.UserName.Equals(user.UserName) && u.UserPassword.Equals(user.UserPassword));
+
+            if (existUser != null)
+            {
+                DateTime expires = DateTime.UtcNow.AddMinutes(2);
+                var token = tr.GenerateToken(user.UserName, expires);
+                return new { authenticated = true, entityId = 1, token = token, tokenExpires = expires };
+            }
+            return new { authenticated = false ,Msg = "用户名或密码错误"};
+        }
+       
+    }
+
+}
